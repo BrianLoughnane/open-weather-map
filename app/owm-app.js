75255c1a22ffedb76b71f34c39de8f6855a066c5
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-29 19:08:57 -0400

    $routeProvider issue

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,34 +1,52 @@
 var OWMapp = angular.module('OWMapp', ['ngRoute']);
 
-OWMapp.value('owmCities', ['New York', 'Dallas', 'Chicago']);
-
 OWMapp.config(function($routeProvider) {
 		$routeProvider.when('/', {
 			templateURL: './home.html',
 			controller: 'HomeCtrl'
-		}).when('/cities/:city', {
-			templateURL: './city.html',
-			controller: 'CityCtrl',
-			resolve: {
-				city: function (owmCities, $route, $location) {
-					var city = $route.current.params.city;
-					if(owmCities.indexOf(city) == -1) {
-						$location.path('/error');
-						return;
-					}
-					return city;
-				}
-			}
-		}).when('/error', {
-			template: '<p>Error Page Not Found</p>'
 		});
-	});
 
 OWMapp.controller('HomeCtrl', function($scope) {
-
+		$scope.test = 'home controller test';
 	});
 
-OWMapp.controller('CityCtrl', function($scope, $routeParams,owmCities) {
-		$scope.city = $routeParams.city;
-	});
+// var OWMapp = angular.module('OWMapp', ['ngRoute']);
+
+// OWMapp.value('owmCities', ['New York', 'Dallas', 'Chicago']);
+
+// OWMapp.config(function($routeProvider) {
+// 		$routeProvider.when('/', {
+// 			templateURL: './home.html',
+// 			controller: 'HomeCtrl'
+// 		}).when('/cities/:city', {
+// 			templateURL: './city.html',
+// 			controller: 'CityCtrl',
+// 			resolve: {
+// 				city: function (owmCities, $route, $location) {
+// 					var city = $route.current.params.city;
+// 					if(owmCities.indexOf(city) == -1) {
+// 						$location.path('/error');
+// 						return;
+// 					}
+// 					return city;
+// 				}
+// 			}
+// 		}).when('/error', {
+// 			template: '<p>Error Page Not Found</p>'
+// 		});
+// 	});
+
+// OWMapp.config(function($routeProvider) {
+// 		$routeProvider.when('/', {
+// 			templateURL: './home.html',
+// 			controller: 'HomeCtrl'
+// 		})
+
+// OWMapp.controller('HomeCtrl', function($scope) {
+
+// 	});
+
+// OWMapp.controller('CityCtrl', function($scope, $routeParams, owmCities) {
+// 		$scope.city = $routeParams.city;
+// 	});
 
